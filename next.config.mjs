41b7c44627060c16853a7698a586bd5c5854e5b5
Dita rector <rectordita@gmail.com>
2026-04-03 20:03:55 +0700

    new structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  images:{
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: "**"
+      }
+    ]
+  }
+};
 
 export default nextConfig;
